8f12d61896581ea3680d7a83eb0a8b45f1e0b1bc
VictorVaquerosoto <54574705+VictorVaquerosoto@users.noreply.github.com>
2019-09-23 21:50:50 +0200

    final plot 3

---
+++ plot3.R
@@ -0,0 +1,43 @@
+##Check if the directory exists, if nor we create it:
+if(!file.exists("./data")){dir.create("./data")}
+
+url<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+##Check if we have already downloaded the zip file, if not we do it:
+if (!file.exists("./data/file.zip")) { 
+  download.file(url, destfile ="./data/file.zip",method="curl")
+}
+
+##Check if we have already unzipped the zip file, if not we do it:
+if (!file.exists("./data/household_power_consumption")) { 
+  unzip(zipfile = "./data/file.zip", exdir = "./data")
+}
+
+#Reading the data file
+data<-read.table("./data/household_power_consumption.txt",sep=";",header=TRUE, stringsAsFactors=FALSE, dec=".")
+#>Convert the date to Date format
+data$Datef <- as.Date(data$Date,"%d/%m/%Y")
+#Set english language
+Sys.setlocale(locale = 'en_US.UTF-8')
+
+#Subset with the appropiate time
+bar <- subset(data, data$Datef =="2007-02-01"|data$Datef=="2007-02-02")
+
+PowerKw <- as.numeric(as.character(bar$Global_active_power))
+Timef <- strptime(bar$Time,"%H:%M:%S")
+Datef <- as.Date(bar$Date,"%d/%m/%Y")
+totaldate<-strptime(paste(bar$Date, bar$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+sub_1<-as.numeric(bar$Sub_metering_1)
+sub_2<-as.numeric(bar$Sub_metering_2)
+sub_3<-as.numeric(bar$Sub_metering_3)
+
+#open the png file
+png("plot3.png", width=480, height=480)
+#define the histogram
+plot(totaldate, sub_1, type="l", xlab="", ylab="Energy sub metering")
+lines(totaldate,sub_2,type ="l",col="red")
+lines(totaldate,sub_3,type="l",col="blue")
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2","Sub_metering_3"),
+       col=c("black","red", "blue"), lty=1)
+#close the png
+dev.off()
