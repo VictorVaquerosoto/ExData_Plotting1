ba5adbc7229e55a0db2572511ef5c92e8cb7b3c0
VictorVaquerosoto <54574705+VictorVaquerosoto@users.noreply.github.com>
2019-09-23 21:50:09 +0200

    final plot 1

---
+++ plot1.R
@@ -0,0 +1,34 @@
+##Check if the directory exists, if nor we create it:
+if(!file.exists("./data")){dir.create("./data")}
+
+url<-"https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+
+##Check if we have already downloaded the zip file, if not we do it:
+if (!file.exists("./data/file.zip")) { 
+  download.file(url, destfile ="./data/file.zip",method="curl")
+}
+
+##Check if we have already unzipped the zip file, if not we do it:
+if (!file.exists("./data/household_power_consumption")) { 
+  unzip(zipfile = "./data/file.zip", exdir = "./data")
+}
+
+#Reading the data file
+data<-read.table("./data/household_power_consumption.txt",sep=";",header=TRUE)
+#>Convert the date to Date format
+data$Datef <- as.Date(data$Date,"%d/%m/%Y")
+#Subset with the appropiate time
+bar <- subset(data, data$Datef =="2007-02-01"|data$Datef=="2007-02-02")
+
+PowerKw <- as.numeric(as.character(bar$Global_active_power))
+#open the png file
+png("plot1.png", width=480, height=480)
+#define the histogram
+hist(PowerKw,col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
+#close the png
+dev.off()
+
+
+
+
+
